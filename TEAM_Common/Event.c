02fb61b1c6771db5f88069681e2f8c5cc62afb61
Adrian Bucher <adrian.bucher@stud.hslu.ch>
2017-10-13 16:24:49 +0200

    Event lab implemented - but not testet yet :P

--- TEAM_Common/Event.c
+++ TEAM_Common/Event.c
@@ -28,35 +28,53 @@ static EVNT_MemUnit EVNT_Events[((EVNT_NOF_EVENTS-1)/EVNT_MEM_UNIT_NOF_BITS)+1];
   (EVNT_Events[(event)/EVNT_MEM_UNIT_NOF_BITS]&(((1u<<(EVNT_MEM_UNIT_NOF_BITS-1))>>(((event)%EVNT_MEM_UNIT_NOF_BITS))))) /*!< Return TRUE if event is set */
 
 void EVNT_SetEvent(EVNT_Handle event) {
-  /*! \todo Make it reentrant */
+
+  CS1_CriticalVariable()					// lokale Variable um vorherige Interrupt-settings zu speichern!
+
+  CS1_EnterCritical();						// schaltet Interrupts aus und speichert aktuelle Interrupt-settings ab
   SET_EVENT(event);
+  CS1_ExitCritical();						// lädt vorherig gespeicherte Interrupt-settings
 }
 
 void EVNT_ClearEvent(EVNT_Handle event) {
-  /*! \todo Make it reentrant */
+
+  CS1_CriticalVariable()
+
+  CS1_EnterCritical();
   CLR_EVENT(event);
+  CS1_ExitCritical();
 }
 
 bool EVNT_EventIsSet(EVNT_Handle event) {
-  /*! \todo Make it reentrant */
-   return GET_EVENT(event);
+  bool isSet;
+
+  CS1_CriticalVariable()
+
+  CS1_EnterCritical();
+  isSet = GET_EVENT(event);
+  CS1_ExitCritical();
+  return isSet;
 }
 
 bool EVNT_EventIsSetAutoClear(EVNT_Handle event) {
   bool res;
+  CS1_CriticalVariable()
 
-  /*! \todo Make it reentrant */
+  CS1_EnterCritical();
   res = GET_EVENT(event);
   if (res) {
     CLR_EVENT(event); /* automatically clear event */
   }
+  CS1_ExitCritical();
   return res;
 }
 
 void EVNT_HandleEvent(void (*callback)(EVNT_Handle), bool clearEvent) {
    /* Handle the one with the highest priority. Zero is the event with the highest priority. */
    EVNT_Handle event;
-   /*! \todo Make it reentrant */
+   CS1_CriticalVariable()
+
+   CS1_EnterCritical();
    for (event=(EVNT_Handle)0; event<EVNT_NOF_EVENTS; event++) { /* does a test on every event */
      if (GET_EVENT(event)) { /* event present? */
        if (clearEvent) {
@@ -65,6 +83,7 @@ void EVNT_HandleEvent(void (*callback)(EVNT_Handle), bool clearEvent) {
        break; /* get out of loop */
      }
    }
+   CS1_ExitCritical();
    if (event != EVNT_NOF_EVENTS) {
      callback(event);
      /* Note: if the callback sets the event, we will get out of the loop.
